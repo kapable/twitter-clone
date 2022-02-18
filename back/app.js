91c962788eaace280c82b496c1b0015683b3eaca
kapable <skjoldenstore@gmail.com>
2022-02-18 18:52:54 +0900

    change back proxy setting

--- back/app.js
+++ back/app.js
@@ -42,6 +42,7 @@ app.use(session({
     saveUninitialized: false,
     resave: false,
     secret: process.env.COOKIE_SECRET,
+    proxy: true,
     cookie: {
         httpOnly: true,
         secure: true,
