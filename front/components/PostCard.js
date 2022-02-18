65de021229c96d2ffbcee281c212d85508d8959c
kapable <skjoldenstore@gmail.com>
2022-02-18 18:47:32 +0900

    fix redux logger and link prefetch

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -94,7 +94,7 @@ const PostCard = ({post}) => {
                         <div style={{ float: 'right' }}>{moment(post.createdAt).format('YYYY.MM.DD')}</div>
                         <Card.Meta
                             avatar={(
-                                    <Link href={`/user/${post.Retweet.User.id}`}>
+                                    <Link href={`/user/${post.Retweet.User.id}`} prefetch={false}>
                                         <a><Avatar>{post.Retweet.User.nickname[0]}</Avatar></a>
                                     </Link>
                                 )}
@@ -108,7 +108,7 @@ const PostCard = ({post}) => {
                         <div style={{ float: 'right' }}>{moment(post.createdAt).format('YYYY.MM.DD')}</div>
                         <Card.Meta
                         avatar={(
-                            <Link href={`/user/${post.User.id}`}>
+                            <Link href={`/user/${post.User.id}`} prefetch={false}>
                                 <a><Avatar>{post.User.nickname[0]}</Avatar></a>
                             </Link>
                             )}
@@ -130,7 +130,7 @@ const PostCard = ({post}) => {
                                 <Comment
                                     author={item.User.nickname}
                                     avatar={(
-                                        <Link href={`/user/${item.User.id}`}>
+                                        <Link href={`/user/${item.User.id}`} prefetch={false}>
                                             <a><Avatar>{item.User.nickname[0]}</Avatar></a>
                                         </Link>
                                     )}
