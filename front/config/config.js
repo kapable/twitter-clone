22b812cee906a8a280e00c4ab872f5d6bb94e622
kapable <skjoldenstore@gmail.com>
2022-02-19 16:54:13 +0900

    fix back api url

--- front/config/config.js
+++ front/config/config.js
@@ -3,5 +3,5 @@ export let backUrl;
 if(process.env.NODE_ENV === 'development') {
     backUrl = 'http://localhost:3065';
 } else {
-    backUrl = 'http://api.jellinggame.net';
+    backUrl = 'https://api.jellinggame.net';
 }
