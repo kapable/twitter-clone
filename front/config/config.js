27f9ab1894079683ed71f7ca8be1cb8e78a560d2
kapable <skjoldenstore@gmail.com>
2022-02-19 16:46:43 +0900

    update dev prod backUrl

--- front/config/config.js
+++ front/config/config.js
@@ -1,1 +1,7 @@
-export const backUrl = 'http://api.jellinggame.net';
+export let backUrl;
+
+if(process.env.NODE_ENV === 'development') {
+    backUrl = 'http://localhost:3065';
+} else {
+    backUrl = 'http://api.jellinggame.net';
+}
--- front/sagas/index.js
+++ front/sagas/index.js
@@ -4,11 +4,7 @@ import postSaga from './post';
 import axios from 'axios';
 import { backUrl } from '../config/config';
 
-if(process.env.NODE_ENV === 'development') {
-    axios.defaults.baseURL = 'http://localhost:3065';
-} else {
-    axios.defaults.baseURL = backUrl;
-}
+axios.defaults.baseURL = backUrl;
 axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
